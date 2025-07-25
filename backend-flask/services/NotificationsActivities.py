997d7870d70ec037adcc442861ae62467014eab6
dev126712 <solonoobhacker6767+@gmail.com>
2025-07-25 17:19:48 +0000

    minor changes

--- backend-flask/services/NotificationsActivities.py
+++ backend-flask/services/NotificationsActivities.py
@@ -5,7 +5,7 @@ class NotificationsActivities:
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'james packer',
-      'message': 'Cloud is fucking hard bro!',
+      'message': 'Cloud is fucking hard brooooooo!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -21,24 +21,5 @@ class NotificationsActivities:
         'reposts_count': 0,
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
-    },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
-      'message': 'I am out of prune juice',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': []
-    },
-    {
-      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
-      'created_at': (now - timedelta(hours=1)).isoformat(),
-      'expires_at': (now + timedelta(hours=12)).isoformat(),
-      'likes': 0,
-      'replies': []
-    }
-    ]
+    }]
     return results
